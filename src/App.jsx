280a6def2b0897ccdc81680adac62ccfdbee8055
Argel Hernandez <argel6767@gmail.com>
2024-06-16 17:23:00 -0400

    intial styling and handleSend()

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import './App.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState([
+    {
+      message: "Hi there! I am ChatGPT",
+      sender: "ChatGPT",
+      direction: "incoming"
+    }
+  ]);
 
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    };
+
+    const newMessages = [...messages, newMessage]; //all old messages plus new one
+  
+    //update message state
+    setMessages(newMessages);
+    //process message to chatGPT
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <div style={{ position: "relative", height:"700px", width:"700px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((message, i) => {
+                return <Message key={i} model = {message}/>
+              })}
+            </MessageList>
+            <MessageInput placeholder='Type your message here' onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+        </div>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    
   )
 }
 
