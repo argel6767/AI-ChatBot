180c0ec3fcbd7e63c04d63aaa0804eba04a82b86
Argel Hernandez <argel6767@gmail.com>
2024-06-19 15:29:10 -0400

    adds import dark statement

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { dark } from "@clerk/themes";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 ReactDOM.createRoot(document.getElementById('root')).render(
